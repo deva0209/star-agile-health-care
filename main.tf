c5b7d56e2f96395153572ab8fac26acf7accb8d3
deva0209 <120808857+deva0209@users.noreply.github.com>
2023-05-15 17:18:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,10 @@ resource "aws_instance" "test-server" {
     private_key = file("gnan.pem")
     host     = self.public_ip
   }
-#provisioner "remote-exec" {
- #   inline = [ "echo 'wait to start instance' "]
-  #}
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory.txt && ansible-playbook playbook.yml"
+  }
   tags = {
     Name = "test-server"
   }
-  provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory.txt "
-		}
-  provisioner "local-exec" {
-  command = "ansible-playbook playbook.yml"
-}
 }
